4582d845cb5d45b374c1ed46f99a8348bf2ec73a
Daniel Budidharma <dharma.daniel2803@gmail.com>
2025-10-23 01:22:16 -0700

    more pages

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,14 +3,24 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+const pages = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Team", path: "/team" },
+    { name: "Contact", path: "/contact" }
+  ];
 
 export default function Navigation() {
+    const pathname = usePathname();
     return (
-        <nav className="flex justify-center items-center p-4">
-            <ul className="flex gap-4">
-                <li><a href="/">Home</a></li>
-                <li><a href="/hello">Hello World!</a></li>
-            </ul>
+        <nav className="flex justify-center items-center p-4 gap-4">
+            {pages.map(({ name, path }) => (
+                <Link href={path} key={path} className={`hover:text-blue-400 ${pathname === path ? "text-blue-400 font-semibold" : ""}`}>
+                    {name}
+                </Link>
+            ))}
         </nav>
     )
 }
