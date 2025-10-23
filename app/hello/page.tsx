e5f74c4a590012cc4dcc0bb23dfad12fec546b01
jeremy <jeremylim158@gmail.com>
2025-10-22 23:41:41 -0700

    i love bhc chicken

---
+++ app/hello/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <nav>
+        <ul><a href="/">Home</a></ul>
+        <ul><a href="/hello">Hello World!</a></ul>
+      </nav>
+      <p>Hello World!</p>
+    </main>
+  );
+}
